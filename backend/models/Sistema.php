08ebefce9db8297c9b8b9d5ea9b8c8b617c336f8
Eiderth <eiderth129@gmail.com>
2026-07-29 12:38:36 -0400

    corregido el bug del login y adaptado a la nueva base de datos

--- backend/models/Sistema.php
+++ backend/models/Sistema.php
@@ -9,25 +9,25 @@ class Sistema {
 
     public function obtener_parametros_formulario() {
         return [
-            'periodos' => $this->db->query('SELECT nombre, id FROM periodos_academicos ORDER BY id DESC')->fetchAll(PDO::FETCH_ASSOC),
-            'secciones' => $this->db->query('SELECT nombre, id FROM secciones')->fetchAll(PDO::FETCH_ASSOC),
-            'grados'  => $this->db->query('SELECT nombre, id FROM grados')->fetchAll(PDO::FETCH_ASSOC),
-            'paises'  => $this->db->query('SELECT nombre, id FROM paises')->fetchAll(PDO::FETCH_ASSOC),
-            'estados'  => $this->db->query('SELECT * FROM estados')->fetchAll(PDO::FETCH_ASSOC),
-            'municipios' => $this->db->query('SELECT * FROM municipios')->fetchAll(PDO::FETCH_ASSOC),
-            'ciudades' => $this->db->query('SELECT * FROM ciudades')->fetchAll(PDO::FETCH_ASSOC),
-            'parroquias' => $this->db->query('SELECT * FROM parroquias')->fetchAll(PDO::FETCH_ASSOC),
+            'periodos' => $this->db->query('SELECT nombre, id FROM PERIODO_ACADEMICO ORDER BY id DESC')->fetchAll(PDO::FETCH_ASSOC),
+            'secciones' => $this->db->query('SELECT nombre, id FROM SECCION')->fetchAll(PDO::FETCH_ASSOC),
+            'grados'  => $this->db->query('SELECT nombre, id FROM GRADO')->fetchAll(PDO::FETCH_ASSOC),
+            'paises'  => $this->db->query('SELECT nombre, id FROM PAIS')->fetchAll(PDO::FETCH_ASSOC),
+            'estados'  => $this->db->query('SELECT * FROM ESTADO')->fetchAll(PDO::FETCH_ASSOC),
+            'municipios' => $this->db->query('SELECT * FROM MUNICIPIO')->fetchAll(PDO::FETCH_ASSOC),
+            'ciudades' => $this->db->query('SELECT * FROM CIUDAD')->fetchAll(PDO::FETCH_ASSOC),
+            'parroquias' => $this->db->query('SELECT * FROM PARROQUIA')->fetchAll(PDO::FETCH_ASSOC),
         ];
     }
 
     public function crear_primer_admin($nombre, $pass) {
-        $tabla_llena = ($this->db->query("SELECT 1 FROM usuarios LIMIT 1")->fetch());
+        $tabla_llena = ($this->db->query("SELECT 1 FROM USUARIO LIMIT 1")->fetch());
 
         if ($tabla_llena) {
             return false; 
         }   
 
-        $stmt = $this->db->prepare("INSERT INTO usuarios (nombre, pass, rol) VALUES (:nombre, :pass, :rol)");
+        $stmt = $this->db->prepare("INSERT INTO USUARIO (nombre, pass, rol) VALUES (:nombre, :pass, :rol)");
 
         $resultado = $stmt->execute([
             'nombre'=> $nombre,
@@ -41,7 +41,7 @@ class Sistema {
     }
 
     public function buscar_usuario($nombre, $pass) {
-        $stmt = $this->db->prepare("SELECT id, nombre, rol FROM usuarios WHERE nombre = :nombre AND pass = :pass");
+        $stmt = $this->db->prepare("SELECT id, nombre, rol FROM USUARIO WHERE nombre = :nombre AND pass = :pass");
         $stmt->execute(['nombre' => $nombre, 'pass' => $pass]);
         return $stmt->fetch();
     }
